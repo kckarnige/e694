0ccdea74ac83ab68ee4138b6bfc9a47ae291e950
KiCKTheBucket <kayos156official@gmail.com>
2025-06-06 21:49:09 -0400

    >:(

--- api/yiff.js
+++ api/yiff.js
@@ -53,7 +53,7 @@ export default async function handler(req, res) {
         sndWarn = `<meta property="og:description" content="🔊 Sound Warning! 🔊" />`
       }
 
-      if (realAuthors == 1) {
+      if (realAuthors.length == 1) {
         postAuthor = `${realAuthors[0]}`
       } else {
         postAuthor = `${realAuthors[0]} +${realAuthors.length - 1}`
