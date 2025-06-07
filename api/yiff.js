9957936e516a8fae859741c5fdfad3b143ff320f
KiCKTheBucket <kayos156official@gmail.com>
2025-06-06 21:10:25 -0400

    What the hell :(

--- api/yiff.js
+++ api/yiff.js
@@ -116,7 +116,7 @@ export default async function handler(req, res) {
 
     const arrayBuffer = await imageResponse.arrayBuffer();
     const buffer = Buffer.from(arrayBuffer);
-    const contentType = imageResponse.headers.get("content-type") || `image/${postInfo.file.ext}`;
+    const contentType = imageResponse.headers.get("content-type") || `image/gif`;
 
     res.setHeader("Content-Type", contentType);
     res.setHeader("Access-Control-Allow-Origin", "*");
