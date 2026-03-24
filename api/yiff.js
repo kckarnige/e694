7ae08a3e14f02dcaf31cad57a86cc71727b24159
KiCKinTheBucket <kayos156official@gmail.com>
2026-03-23 23:18:10 -0400

    I hate ts dawg

--- api/yiff.js
+++ api/yiff.js
@@ -1,7 +1,7 @@
 export default async function handler(req, res) {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "GET, OPTIONS");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Accept");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Accept, User-Agent");
 
   if (req.method === "OPTIONS") {
     return res.status(204).end();
